a0f72d7acb45f10952902cbf40d45d3b4199e9cf
Ricky Reusser <rsreusser@gmail.com>
2017-04-06 02:49:17 -0400

    🦁

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Plotly = window.Plotly = require('plotly.js');
 var gd = window.gd = document.createElement('div');
 document.body.appendChild(gd);
 
-fetch('./assets/airfoil.json', function (resp) {
+fetch('assets/airfoil.json', function (resp) {
   resp.json().then(function (mock) {
     window.mock = mock;
     Plotly.plot(gd, mock)
